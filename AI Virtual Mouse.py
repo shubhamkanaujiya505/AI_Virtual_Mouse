54e4c3af20ec73d9d3518d7868df9e14db23d30c
Shubham Kanaujiya <shubhamkanaujiya505@gmail.com>
2022-04-27 20:53:41 +0530

    initial commit Adding camera detector

---
+++ AI Virtual Mouse.py
@@ -0,0 +1,21 @@
+from sre_constants import SUCCESS
+import cv2
+import numpy as np
+import HandTrackingModule as htm
+import time
+import autopy
+
+
+wCam, hCam = 640, 480
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+
+while True:
+    success, img = cap.read()
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
