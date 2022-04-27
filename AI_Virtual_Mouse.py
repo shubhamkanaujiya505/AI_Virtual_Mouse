48bb71cf21bf39b152f9c37f124a59e99451d915
Shubham Kanaujiya <shubhamkanaujiya505@gmail.com>
2022-04-27 23:47:19 +0530

    Adding Frame Rate

--- AI_Virtual_Mouse.py
+++ AI_Virtual_Mouse.py
@@ -1,4 +1,3 @@
-from sre_constants import SUCCESS
 import cv2
 import numpy as np
 import HandTrackingModule as htm
@@ -12,10 +11,15 @@ wCam, hCam = 640, 480
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
+pTime = 0
 
 
 while True:
     success, img = cap.read()
+    cTime = time.time()
+    fps = 1/(cTime - pTime) 
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 58), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3) 
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
